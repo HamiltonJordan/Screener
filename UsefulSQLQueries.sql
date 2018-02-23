750c6b57bd8c2fe1aa006744acc1858917a30825
jordanhelpsalot <hamilton_jordan@wheatoncollege.edu>
2018-02-23 15:21:58 -0500

    Adding SQL database and projectlog file

--- UsefulSQLQueries.sql
+++ UsefulSQLQueries.sql
@@ -1,33 +1,28 @@
--- ===================================================
--- These are some useful queries we can use as we 
--- develop out our database and need to test out
--- functionality
---							- Danny
--- ===================================================
+
 
 -- Gets all students and what classes they are in
-SELECT e.StudentId, s.FirtName, s.LastName, c.ClassName
-FROM EnrolledIn e
-INNER JOIN Student s ON e.StudentId = s.StudentId
-INNER JOIN Class c ON e.ClassId = c.ClassId
-WHERE s.Active = 1
-AND   c.Active = 1
+SELECT EnrolledIn.StudentId, Student.FirstName, Student.LastName, Class.ClassName
+FROM EnrolledIn
+INNER JOIN Student ON EnrolledIn.StudentId = Student.Id
+INNER JOIN Class ON EnrolledIn.ClassId = Class.Id
+WHERE Student.Active = 1
+AND   Class.Active = 1;
 -- AND e.ClassId = 1  --This filters it to a specific class
 
 -- Gets all professor and what classes they teach
-SELECT t.ProfessorId, p.FirtName, p.LastName, c.ClassName
-FROM Teaches t
-INNER JOIN Professor p ON t.StudentId = p.ProfessorId
-INNER JOIN Class c ON t.ClassId = c.ClassId
-WHERE p.Active = 1
-AND   c.Active = 1
+SELECT Teaches.ProfessorId, Professor.FirtName, Professor.LastName, Class.ClassName
+FROM Teaches
+INNER JOIN Professor ON Teaches.ProfessorId = Professor.Id
+INNER JOIN Class ON Teaches.ClassId = Class.Id
+WHERE Professor.Active = 1
+AND   Class.Active = 1;
 -- AND t.ClassId = 1  --This filters it to a specific class
 
 -- Gets all videos and what classes they are for
-SELECT cv.VideoId, v.Title, v.URL, c.ClassName
-FROM ClassVideo cv
-INNER JOIN Video v ON cv.VideoId = v.VideoId
-INNER JOIN Class c ON cv.ClassId = c.ClassId
-WHERE p.Active = 1
-AND   c.Active = 1
+SELECT ClassVideo.VideoId, Video.Title, Video.URL, Class.ClassName
+FROM ClassVideo
+INNER JOIN Video ON ClassVideo.VideoId = Video.Id
+INNER JOIN Class ON ClassVideo.ClassId = Class.Id
+WHERE Video.Active = 1
+AND   Class.Active = 1
 -- AND cv.ClassId = 1  --This filters it to a specific class
