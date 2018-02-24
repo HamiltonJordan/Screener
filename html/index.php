8e5e07db7c097a5343b178aa51d3d86e9225690b
jordanhelpsalot <hamilton_jordan@wheatoncollege.edu>
2018-02-24 16:41:36 -0500

    restructured repo to embed in the server root - html directory will be hosted

---
+++ html/index.php
@@ -0,0 +1,28 @@
+<?php
+$username = "root";
+$password = "jthklo123";
+$servername = "localhost"; 
+$conn = new mysqli($servername, $username, $password,"websitedb");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$sql = "SELECT *  FROM user";
+$result = $conn->query($sql);
+
+
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $show= " id: ". $row["id"]. " - Type: ". $row["usertype"].$show;
+    }
+} else {
+    $show= "0 results";
+}
+
+echo  json_encode($show);
+?>
