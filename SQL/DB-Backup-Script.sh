e5629c6477353dc356fe8881db53fa1cf996e30b
Daniel Jay Barber <barber_daniel@wheatoncollege.edu>
2018-02-25 21:45:49 -0500

    Create DB-Backup-Script.sh

---
+++ SQL/DB-Backup-Script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Database credentials
+user="root"
+password="jthklo123"
+host="localhost"
+db_name="websitedb"
+
+# Other options
+backup_path="/home/jordan/Desktop"
+date=$(date +"%d-%b-%Y")
+
+# Set default file permissions
+umask 177
+
+# Dump database into SQL file
+mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql
