ad23f64078b3772a66c326342a27cb98aa93eb2e
Daniel Barber <danielbarber@daniels-mbp.wheatonma.edu>
2018-02-28 20:45:41 -0500

    clean up

--- TestUploadFiles/index.js
+++ TestUploadFiles/index.js
@@ -14,23 +14,31 @@ $(document).ready(function() {
         uploadImage($form);
     });
     function uploadImage($form){
-        $form.find('.progress-bar').removeClass('progress-bar-success')
-                                    .removeClass('progress-bar-danger');
-        var formdata = new FormData($form[0]); //formelement
+        $form.find('.progress-bar').removeClass('progress-bar-success')  // Not Needed
+                                    .removeClass('progress-bar-danger'); // Not Needed
+
+        // Needed
+        var formdata = new FormData($form[0]); //formelement {data}
         var request = new XMLHttpRequest();
+
+
         //progress event...
-        request.upload.addEventListener('progress',function(e){
-            var percent = Math.round(e.loaded/e.total * 100);
-            $form.find('.progress-bar').width(percent+'%').html(percent+'%');
+        request.upload.addEventListener('progress',function(e){          // Not Needed
+            var percent = Math.round(e.loaded/e.total * 100);            // Not Needed
+            $form.find('.progress-bar').width(percent+'%').html(percent+'%'); // Not Needed
         });
         //progress completed load event
-        request.addEventListener('load',function(e){
+        request.addEventListener('load',function(e){                    // Not Needed 
             $form.find('.progress-bar').addClass('progress-bar-success').html('upload completed....');
         });
+
+        // Needed
         request.open('post', 'server.php');
         request.send(formdata);
         $form.on('click','.cancel',function(){
             request.abort();
+            
+            // Not Needed
             $form.find('.progress-bar')
                 .addClass('progress-bar-danger')
                 .removeClass('progress-bar-success')
