573a6ccd539aecbcad6867c2557e5315ee78c9b8
korechia <orechia_kathleen@wheatoncollege.edu>
2018-03-04 14:54:26 -0500

    works testdynvid2 to getvid.php to testdynvid.html

--- html/getvideo.php
+++ html/getvideo.php
@@ -1,18 +1,19 @@
 <?php
-    $video=$_GET["video"]; 
-
     session_start();
+    $video=$_GET["video"];
+	$title=$_GET["titlev"]; 
 
 
     if($video=="already"){
-    $video=$_SESSION['savedv'];
+    $video=$_SESSION['saved'];
+ $title=$_SESSION['savedt'];
 }
     if ($video!="already"){
-    $_SESSION['savedv']=$video;
-}
-    
-$obj1 = (object)array("title"=>$video);
-     echo json_encode($obj1); 
+    $_SESSION['saved']=$video;
+$_SESSION['savedt']=$title;
+}  
+$obj1 = (object)array("title"=> $title,"video"=> $video);
+echo  json_encode($obj1); 
 
 
 ?>
---
+++ html/testvideo.php
@@ -0,0 +1,8 @@
+<?php
+    $video=$_GET["video"]; 
+    echo "Video: " + $video;
+
+    #session_start();
+    echo "Session: :"+ $_SESSION['saved'];
+
+?>
