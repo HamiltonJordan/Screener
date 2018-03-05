c1f754e4c71913e17cf7d2cedbb4412e566a1140
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-03-04 19:42:28 -0500

    cleaning and progress on dynamic video switching - changed dynvid pages to selector and viewer and added fetchMyFilms.php

---
+++ html/fetchMyFilms.php
@@ -0,0 +1,38 @@
+<?php
+	//This code does not work yet!
+	error_reporting(E_ALL); 
+	ini_set('display_errors',1);
+
+	$servername = "127.0.0.1";
+	$username = "root";
+	$password = "jthklo123";
+	$dbname = 'websitedb';
+
+	class ReturnObject {
+		public $success = false;
+		public $loginCheck = false;
+	}
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	//Psuedo-Code
+	$classID = $_SESSION($classID);
+
+	$returnObj = new ReturnObject();
+	if ($result = $conn->query("SELECT * FROM Video WHERE ClassID = '$classID'")) {
+		while ($row = mysqli_fetch_assoc($result)) {
+		}
+	
+		/* free result set */
+		$result->close();
+	}
+
+	mysqli_close($conn);
+	echo json_encode($returnObj);
+
+?>
--- html/getvideo.php
+++ html/getvideo.php
@@ -1,19 +1,19 @@
 <?php
-    session_start();
-    $video=$_GET["video"];
+	session_start();
+	$video=$_GET["video"];
 	$title=$_GET["titlev"]; 
 
 
-    if($video=="already"){
-    $video=$_SESSION['saved'];
- $title=$_SESSION['savedt'];
-}
-    if ($video!="already"){
-    $_SESSION['saved']=$video;
-$_SESSION['savedt']=$title;
-}  
-$obj1 = (object)array("title"=> $title,"video"=> $video);
-echo  json_encode($obj1); 
+	if($video=="already"){
+		$video=$_SESSION['saved'];
+		$title=$_SESSION['savedt'];
+	}
+	if ($video!="already"){
+		$_SESSION['saved']=$video;
+		$_SESSION['savedt']=$title;
+	}  
+	$obj1 = (object)array("title"=> $title,"video"=> $video);
+	echo  json_encode($obj1); 
 
 
 ?>
