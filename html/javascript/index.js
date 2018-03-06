417ee3bfc45766a3de8adeb7dde59e1e27ed45a6
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-03-06 14:38:29 -0500

    more fetch work

--- html/javascript/index.js
+++ html/javascript/index.js
@@ -9,7 +9,7 @@ $("#submit").click(function() {
 		var myObj = JSON.parse(response);
 		if (myObj.success && myObj.loginCheck) {
 			console.log('here');
-			document.location.replace('screener.onthewifi.com/selector.html');
+			document.location.replace('selector.html');
 		
 		}
 		else {
