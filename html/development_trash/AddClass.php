6cbc6c5a3796f2bf422ce7dfd43290ace265418f
Daniel Barber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-07 16:18:19 -0500

    add class

--- html/development_trash/AddClass.php
+++ html/development_trash/AddClass.php
@@ -28,7 +28,7 @@
 
     if(is_array($studentIds)) {
         foreach($studentIds as $Id) {
-            $query1 = mysqli_real_escape_string($Id);
+            $query1 = mysqli_real_escape_string($conn, $Id);
             $query ="INSERT INTO EnrolledIn (ClassId, StudentId) VALUES ( '". $classId."','".$Id."' )";
             mysqli_query($conn, $query);
         }
