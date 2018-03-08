849bf8d09af160ddb534786343dd95e8aa5596ce
Daniel Barber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-08 14:50:44 -0500

    DB/PHP cleanup

--- html/Development/DBConnect.php
+++ html/Development/DBConnect.php
@@ -2,29 +2,21 @@
 function db_connect() {
 
     // Define connection as a static variable, to avoid connecting more than once 
-    static $connection;
+    static $conn;
 
     // Try and connect to the database, if a connection has not been established yet
-    if(!isset($connection)) {
+    if(!isset($conn)) {
         // Load configuration as an array. Use the actual location of your configuration file
-        $config = include("config.php"); // Config file URL goes here 
-        $connection = mysqli_connect($config->servername,$config->username,$config->password,$config->dbname);
+        $config = include("/home/screener/Config/config.php"); // Config file URL goes here 
+        $conn = mysqli_connect($config->servername,$config->username,$config->password,$config->dbname);
     }
 
     // If connection was not successful, handle the error
-    if($connection === false) {
+    if($conn === false) {
         // Handle error - notify administrator, log to a file, show an error screen, etc.
         return mysqli_connect_error(); 
     }
-    return $connection;
-}
-
-// Connect to the database
-$connection = db_connect();
-
-// Check connection
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
+    return $conn;
 }
 ?> 
 
