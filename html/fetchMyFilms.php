a5baee7c1044fb4956e074d9aa6d036a3e6930b6
Daniel Barber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-08 14:32:08 -0500

    db/php changes

--- html/fetchMyFilms.php
+++ html/fetchMyFilms.php
@@ -3,22 +3,20 @@
 	error_reporting(E_ALL); 
 	ini_set('display_errors',1);
 
-	require('DBConnect.php');
+	$servername = "127.0.0.1";
+	$username = "root";
+	$password = "jthklo123";
+	$dbname = 'websitedb';
 
-	// $servername = "127.0.0.1";
-	// $username = "root";
-	// $password = "jthklo123";
-	// $dbname = 'websitedb';
-
-	$userId="1";  //$_GET["userId"];
+	$userId=$_GET["userId"];
 	$return_arr = array();
 	
 	// Create connection
-	//$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
-	// if ($conn->connect_error) {
-	// 	die("Connection failed: " . $conn->connect_error);
-	// }
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
 
 	if ($result = $conn->query("
 		SELECT Video.Title, Video.URL FROM User
