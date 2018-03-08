ea84cfb8e3b2317f3ae3d909f1e1d5fa5932ee67
Daniel Barber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-08 15:18:59 -0500

    CSS loging page

--- html/javascript/index.js
+++ html/javascript/index.js
@@ -1,17 +1,29 @@
-//splash.js
+$(document).ready(function() {
+	$(".create-account-form").hide();
 
-$("#submit").click(function() {
-	var user = $("#email").val();
-	var pass = $("#password").val();
-	$.get("login.php?email="+user+"&password="+pass, function(response){
-		var myObj = JSON.parse(response);
-		if (myObj.success && myObj.loginCheck) {
-			Cookies.remove('loginId');
-			Cookies.set('loginId', myObj.id);
-			document.location.replace('selector.html');
-		}
-		else {
-			alert('failed to login.');
-		}
+	// Account Creation Form Show
+	$("#createAccountBtn").click(function() {
+		$(".login-form").hide();
+		$(".create-account-form").show();
 	});
-});
+
+
+	$("#submit").click(function() {
+		var user = $("#email").val();
+		var pass = $("#password").val();
+		$.get("login.php?email="+user+"&password="+pass, function(response){
+			var myObj = JSON.parse(response);
+			if (myObj.success && myObj.loginCheck) {
+				Cookies.remove('loginId');
+				Cookies.set('loginId', myObj.id);
+				document.location.replace('selector.html');
+			}
+			else {
+				alert('failed to login.');
+			}
+		});
+	});
+
+
+
+}); // End of Document Ready
