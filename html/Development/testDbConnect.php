dc6fda76383a07f07fe5f61b156346714d4184c3
Daniel Barber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-08 14:42:32 -0500

    DB/PHP cleanup

--- html/Development/testDbConnect.php
+++ html/Development/testDbConnect.php
@@ -2,39 +2,8 @@
 
     $conn = include("DBConnect.php");
 
-    $userId = "1";
+    $sql = "SELECT *  FROM Video";
+    $result = $conn->query($sql);
 
-    if ($result = $conn->query("
-		SELECT Video.Title, Video.URL FROM User
-		INNER JOIN EnrolledIn ON
-		User.Id = EnrolledIn.UserId
-		INNER JOIN ClassVideo ON
-		EnrolledIn.ClassId = ClassVideo.ClassId
-		INNER JOIN Video ON
-		ClassVideo.VideoId = Video.Id
-		WHERE User.Id = '$userId';
-		")) 
-	{
-		while ($row = mysqli_fetch_assoc($result)) {
-			$row_array['Title'] = $row['Title'];
-			$row_array['URL'] = $row['URL'];
-			array_push($return_arr, $row_array);
-			/*
-			echo "Title: ";
-			echo $row['Title'];
-			echo ", Location: ";
-			echo $row['URL'];
-			echo ".......";
-			*/
-		}
-	
-		/* free result set */
-		$result->close();
-	}
-
-	mysqli_close($conn);
-	echo json_encode($return_arr);
-	//$returnObj = new ReturnObject();
-	//echo json_encode($returnObj);
-
+    echo $result;
 ?>
