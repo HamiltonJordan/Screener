9b784d341f7851cb5f0e6602f26577b9fadd4c34
Daniel Barber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-08 15:21:04 -0500

    Undoing login page css

--- html/javascript/index.js
+++ html/javascript/index.js
@@ -1,29 +1,17 @@
-$(document).ready(function() {
-	$(".create-account-form").hide();
+//splash.js
 
-	// Account Creation Form Show
-	$("#createAccountBtn").click(function() {
-		$(".login-form").hide();
-		$(".create-account-form").show();
+$("#submit").click(function() {
+	var user = $("#email").val();
+	var pass = $("#password").val();
+	$.get("login.php?email="+user+"&password="+pass, function(response){
+		var myObj = JSON.parse(response);
+		if (myObj.success && myObj.loginCheck) {
+			Cookies.remove('loginId');
+			Cookies.set('loginId', myObj.id);
+			document.location.replace('selector.html');
+		}
+		else {
+			alert('failed to login.');
+		}
 	});
-
-
-	$("#submit").click(function() {
-		var user = $("#email").val();
-		var pass = $("#password").val();
-		$.get("login.php?email="+user+"&password="+pass, function(response){
-			var myObj = JSON.parse(response);
-			if (myObj.success && myObj.loginCheck) {
-				Cookies.remove('loginId');
-				Cookies.set('loginId', myObj.id);
-				document.location.replace('selector.html');
-			}
-			else {
-				alert('failed to login.');
-			}
-		});
-	});
-
-
-
-}); // End of Document Ready
+});
