51414a6a17b0154fc64364bcd8d1402523f3fea8
Daniel Barber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-08 14:45:18 -0500

    DB/PHP cleanup

--- html/Development/testDbConnect.php
+++ html/Development/testDbConnect.php
@@ -1,6 +1,14 @@
 <?php
 
-    $conn = include("DBConnect.php");
+    require("DBConnect.php");
+
+    // Connect to the database
+$conn = db_connect();
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
     $sql = "SELECT *  FROM Video";
     $result = $conn->query($sql);
