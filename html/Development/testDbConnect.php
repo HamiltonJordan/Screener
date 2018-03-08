be2c1511e3c718dd23cbe5dc2492a868c078c10f
Daniel Barber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-08 14:46:25 -0500

    DB/PHP cleanup

--- html/Development/testDbConnect.php
+++ html/Development/testDbConnect.php
@@ -9,9 +9,50 @@ $conn = db_connect();
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+echo "Start videotest.php";
+class Video {
+  public $Title = "";
+  public $URL = "";
+}
 
-    $sql = "SELECT *  FROM Video";
-    $result = $conn->query($sql);
+class ReturnObject {
+  public $success = false;
+  public $videos = "";
+  public $error = "";
+}
 
-    echo $result;
+$sql = "SELECT * FROM Video";
+$result = $conn->query($sql);
+
+
+$videoArray = [];
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+    	$video = new Video();
+    	$video->Title = $row["Title"];
+     	$video->URL = $row["URL"];
+	array_push($videoArray, $video);
+    }
+    if (count($videoArray) > 0) {
+ 	$returnObj = new ReturnObject();
+    	$returnObj->success = True;
+    	$returnObj->vidoes = $videoArray;
+    	echo json_encode($returnObj);
+    }
+    else {
+    	$returnObj = new ReturnObject();
+    	$returnObj->success = False;
+    	$returnObj->error = "Videos found in database, but fetch/push failed";
+    	echo json_encode($returnObj);
+    }
+} 
+else {
+    $returnObj = new ReturnObject();
+    $returnObj->success = False;
+    $returnObj->error = "No videos found in database";
+    echo json_encode($returnObj);
+}
+$conn->close();
 ?>
