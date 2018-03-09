25c88273923d91f76a55bc3a0f3103f2882b1031
danieljaybarber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-08 19:05:40 -0500

    Add class work

--- html/AddClass.php
+++ html/AddClass.php
@@ -24,10 +24,16 @@
     $returnObj = new ReturnObject();
     if(is_array($studentIds)) {
         foreach($studentIds as $Id) {
-            $query1 = mysqli_real_escape_string($conn, $Id);
-            $query ="INSERT INTO EnrolledIn (UserId, ClassId) VALUES ( '". $Id."','".$classId."' )";
-            mysqli_query($conn, $query);
-            $returnObj->rowCount = $returnObj->rowCount + 1;
+
+            $result = mysql_query("SELECT Id FROM Users WHERE WheatonId='$Id' LIMIT 1");
+            $row = mysql_fetch_assoc($result);
+            echo $row['Id'];
+
+
+            // $query1 = mysqli_real_escape_string($conn, $Id);
+            // $query ="INSERT INTO EnrolledIn (UserId, ClassId) VALUES ( '". $Id."','".$classId."' )";
+            // mysqli_query($conn, $query);
+            // $returnObj->rowCount = $returnObj->rowCount + 1;
         }
         $returnObj->success = true;
     }
