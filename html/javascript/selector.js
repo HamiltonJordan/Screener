60defed670aa8a7a6f4a0c740075e49dcbdaeb53
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-03-12 07:25:23 -0400

    organized viewer into javascript and css files

--- html/javascript/selector.js
+++ html/javascript/selector.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
-	//var url = window.location.href;
-	//var urlList = url.split("=");
-	//var myId = urlList[1];
 	var myId = Cookies.get('loginId');
 	//PHP - request the JSON list of film information available to user
 	$.get("fetchMyFilms.php?userId="+myId, "", function (response){
 		myFilms=JSON.parse(response);
 		console.log("response: " + response);
 		console.log("myFilms: " + myFilms);
-
+		//For every film in the list returned, create a cell in the table
 		for (i = 0; i < myFilms.length; i++){
 			$("#selector_table").append('<tr><td class="Title"><a href="viewer.html" id="a'+i+'">'+myFilms[i].Title+'</a></td><td>'+myFilms[i].URL+'</td></tr>');
 			$("#a"+i).data("URL", myFilms[i].URL);
