476cd6b30d0d71180c27497794000b6182343cd8
danieljaybarber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-22 10:26:23 -0400

    Professor Class List

--- html/Development/ProfessorClassList.js
+++ html/Development/ProfessorClassList.js
@@ -5,10 +5,18 @@ $(document).ready(function() {
 
 	//PHP - request the JSON list of film information available to user
 	$.get("fetchMyClasses.php?userId="+myId, "", function (response){
-		myFilms=JSON.parse(response);
+		myClasses=JSON.parse(response);
 		
-		console.log(response);
-		console.log(myFilms);
+		$last_class="null";
+		//For every film in the list returned, create a cell in the table
+		for (i = 0; i < myClasses.length; i++){
+			if(myFilms[i].ClassName != $last_class){
+				$("#entry-point").append('<tr class="table-primary"><th scope="row" colspan="4">'+myClasses[i].ClassName+'</th></tr>');
+				$last_class = myClasses[i].ClassName;
+			}
+			$("#entry-point").append('<tr class="movie-cell"><th scope="row">'+(i+1)+'</th><td class="Title"><a href="viewer.html" id="a'+i+'">'+myClasses[i].ClassName+'</a></td><td>1:11</td><td>3/31/18</td></tr>');
+			$("#a"+i).data("URL", myClasses[i].ClassNumber);
+		}
 	});		
 
 }); // End of Document Ready
