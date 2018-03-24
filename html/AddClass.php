ae2bbfc85b99a692d6760b3a4e639a4e96a9d59c
danieljaybarber <barber_daniel@wheatoncollege.edu>
2018-03-24 17:09:03 -0400

    add class changes

--- html/AddClass.php
+++ html/AddClass.php
@@ -29,8 +29,9 @@
         $active = 1;
         $query1 = mysqli_real_escape_string($conn, $classTitle);
         $query2 = mysqli_real_escape_string($conn, $classCode);
-        $query ="INSERT INTO Class (ClassName, ClassNumber, Active) VALUES ( '". $classTitle."','".$classCode."','".$active."' )";
+        $query ="INSERT INTO 'Class' (ClassName, ClassNumber, Active) VALUES ( '". $classTitle."','".$classCode."','".$active."' )";
         mysqli_query($conn, $query);
+        echo $mysqli->error;
         $returnObj->rowCount = $classId; //Delete
         $returnObj->success = true;     // Delete
     }
