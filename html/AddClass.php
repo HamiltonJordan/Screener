7b95e7088ce2ac4f0b3aa67e556497e7c4f1f5b9
danieljaybarber <danielbarber@daniels-mbp.wheatonma.edu>
2018-03-24 14:28:41 -0400

    Add Class work

--- html/AddClass.php
+++ html/AddClass.php
@@ -17,34 +17,59 @@
 
     $studentIds = $classData->studentList;
 
-    $classId = 69;
-
     $counter = 0;
+    $classId = 0;
 
     $returnObj = new ReturnObject();
-    if(is_array($studentIds)) {
-        foreach($studentIds as $wheatonId) {
 
-            if ($result = $conn->query("
+    // Adding new class to Class table
+    if($classData->classTitle != '' && $classData->classCode != '') {
+        $query1 = mysqli_real_escape_string($conn, $Id);
+        $query ="INSERT INTO Class (ClassName, ClassNumber, Active) VALUES ( '". $classData->classTitle."','".$classData->classCode."','".$classData->Active."' )";
+        mysqli_query($conn, $query);
+    }
+
+    // Getting newly created classe's Id
+    if ($result = $conn->query("
                 SELECT Id
-                From User
-                WHERE User.WheatonId = '$wheatonId'
+                From Class
+                WHERE Class.ClassName = '$classData->classTitle'
+                AND Class.ClassNumber = '$classData->classCode'
+                AND Class.Active = '$classData->Active'
                 Limit 1;
                 ")) 
-            {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $Id = $row['Id'];
-                    $query1 = mysqli_real_escape_string($conn, $Id);
-                    $query ="INSERT INTO EnrolledIn (UserId, ClassId) VALUES ( '". $Id."','".$classId."' )";
-                    mysqli_query($conn, $query);
-                    $returnObj->rowCount = $returnObj->rowCount + 1;
-                            
-                }
-            }
-            
-        }
-        $returnObj->success = true;
+    {
+       while ($row = mysqli_fetch_assoc($result)) {
+            $classId = $row['Id'];
+            $returnObj->rowCount = $classId //Delete
+            $returnObj->success = true;     // Delete
+       } 
     }
+
+    // // Adds Students to class under EnrolledIn table
+    //if(is_array($studentIds)) {
+    //     foreach($studentIds as $wheatonId) {
+
+    //         if ($result = $conn->query("
+    //             SELECT Id
+    //             From User
+    //             WHERE User.WheatonId = '$wheatonId'
+    //             Limit 1;
+    //             ")) 
+    //         {
+    //             while ($row = mysqli_fetch_assoc($result)) {
+    //                 $Id = $row['Id'];
+    //                 $query1 = mysqli_real_escape_string($conn, $Id);
+    //                 $query ="INSERT INTO EnrolledIn (UserId, ClassId) VALUES ( '". $Id."','".$classId."' )";
+    //                 mysqli_query($conn, $query);
+    //                 $returnObj->rowCount = $returnObj->rowCount + 1;
+                            
+    //             }
+    //         }
+            
+    //     }
+      //  $returnObj->success = true;
+    //}
 
     echo json_encode($returnObj);
 
