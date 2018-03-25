9de44f805a90aa5aea7693e4533d3fb6a7d1fc27
korechia <orechia_kathleen@wheatoncollege.edu>
2018-03-24 23:35:13 -0400

    testing

--- html/Development/uploader.js
+++ html/Development/uploader.js
@@ -47,6 +47,7 @@ $("#add").click(function(){
     $(document).on('submit','form',function(e){
         e.preventDefault();
         $form = $(this);
+	var run=false;
  if($form.children("input").filter('[name=name]').val() ==""){
         $form.children("input").filter('[name=name]').css('border-color', 'red');
         run=false;
