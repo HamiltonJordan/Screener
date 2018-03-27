5313ba3ae98bc25cbdc14b2007495b136813dcea
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-03-27 17:06:59 -0400

    Continuing on instructor

--- html/javascript/instructor.js
+++ html/javascript/instructor.js
@@ -56,9 +56,7 @@ function populateStuds(target_class){
 //alert(myClass[0].student[3].FirstName);
 $(document).ready(function () {
 	function populateClass(response){
-		alert("inside");
 		for (var i = 0; i < $myClasses.ClassList.length; i++){
-			alert("in loop");
 			$("#class-entry-point").append('<button type="button" class="btn btn-secondary classButton" id="b'+i+'">'+$myClasses.ClassList[0].ClassNumber+'</button>');
 		}
 	}
