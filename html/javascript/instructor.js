be8322417c92c97ebfca9c1541aa52dc62e11616
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-03-29 22:25:25 -0400

    css work on instructor

--- html/javascript/instructor.js
+++ html/javascript/instructor.js
@@ -48,10 +48,10 @@ function populateStuds(target_class){
 	});
 }
 function AJAX_refreshClasses(argument){
+	var myId = 4; //Gousie
 	$.get("http://screener.onthewifi.com/instructor.php?instructorId="+myId)
 		.done(function (response){
 			alert("ey");
-			/*
 			$myClasses = JSON.parse(response);
 			populateClass();
 			$class_selected = null;
@@ -71,7 +71,6 @@ function AJAX_refreshClasses(argument){
 					$("#add-field").show(500);
 				}
 			});
-			*/
 		})
 		.fail(function (){
 			alert("failed to connect to the database");
@@ -81,8 +80,6 @@ function AJAX_refreshClasses(argument){
 
 //alert(myClass[0].student[3].FirstName);
 $(document).ready(function () {
-
-	var myId = 4; //Gousie
 	alert("stop");
 	AJAX_refreshClasses();
 	$('#submit-student').click(function() {
