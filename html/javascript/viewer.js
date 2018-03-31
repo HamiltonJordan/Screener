9311dc151fcf345bff1b4ba8f9dc3806ad09eb57
korechia <orechia_kathleen@wheatoncollege.edu>
2018-03-31 18:43:45 -0400

    sessions

--- html/javascript/viewer.js
+++ html/javascript/viewer.js
@@ -1,4 +1,9 @@
-$url = Cookies.get('URL');
+ $.get("getvideo.php?video=already&titlev=already", "", function (response){
+        json=JSON.parse(response);
+        alert("already " +json.video);
+
+//$url = Cookies.get('URL');
 $('#portal').get(0).pause(); 
-$('#video_address').attr('src', $url);
+$('#video_address').attr('src', json.video);
 $('#portal').get(0).load();
+}
