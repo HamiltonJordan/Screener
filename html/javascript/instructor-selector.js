0038a59a7035e697d6826025e8e0c6f288c2de51
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-04-06 14:16:02 -0400

    adding php for prof class readout

---
+++ html/javascript/instructor-selector.js
@@ -0,0 +1,52 @@
+//Instructor-Selector.js
+$(document).ready(function () {
+	//var myId = Cookies.get('loginId');
+
+	//PHP - request the JSON list of film information available to user
+	$.get("http://screener.onthewifi.com/fetchClassFilms.php", "", function (response){
+		myFilms=JSON.parse(response);
+		//myFilms = {}
+		console.log("response: " + response);
+		alert("myFilms: " + myFilms);
+		$last_class="null";
+		//For every film in the list returned, create a cell in the table
+		for (i = 0; i < myFilms.length; i++){
+			if(myFilms[i].ClassName != $last_class){
+				$("#entry-point").append('<tr class="table-primary"><th scope="row" colspan="4">'+myFilms[i].ClassName+'</th></tr>');
+				$last_class = myFilms[i].ClassName;
+			}
+			$("#entry-point").append('<tr class="movie-cell"><th scope="row">'+(i+1)+'</th><td class="Title"><a href="viewer.html" id="a'+i+'">'+myFilms[i].Title+'</a></td><td>1:11</td><td>3/31/18</td></tr>');
+			$("#a"+i).data("URL", myFilms[i].URL);
+		}
+		$("#a"+i).click({p1:myFilms[i].URL,p2:myFilms[i].Title},function(event){
+			url=event.data.p1;
+			title=event.data.p2;
+			$.get("getvideo.php?video="+url+"&titlev="+title, "", function (response){
+				json=JSON.parse(response);
+			})
+		})
+	});
+	/*
+	//Sample Json Obj
+	var jsonOBJ = {
+		"class": [
+			{
+				"name":"Biology",
+				"films": [
+					{"Title": "T1", "URL": "/source.mp4"},
+					{"Title": "T2", "URL": "/othersource.mp4"}
+				]
+			},	
+				{
+				"name":"English",
+				"films": [
+					{"Title": "T3", "URL": "/Newsource.mp4"},
+					{"Title": "T4", "URL": "/Whatsource.mp4"}
+				]
+			}
+		]
+	};
+	alert(jsonOBJ.class[0].films[1].Title);
+	*/
+
+});
