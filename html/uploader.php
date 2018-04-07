14972e5b477d75bac77cab7a5ad7c4b2874e45da
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-04-07 14:47:11 -0400

    adding fix for sessions in addclass.php

--- html/uploader.php
+++ html/uploader.php
@@ -7,7 +7,7 @@
 	//		-UploadFolder/TeacherUserName/ClassCode/$filename
 	// This will ensure that we keep all files organized on server while uploading.
 $t=time();
-move_uploaded_file($tmp_file,'upload_folder/'.$filename.$t)
+move_uploaded_file($tmp_file,'video/'.$filename.$t)
 
 	$name=$_POST['name'];
 	$duedate=$_POST['duedate'];
@@ -16,24 +16,25 @@ move_uploaded_file($tmp_file,'upload_folder/'.$filename.$t)
  // Connect to the database
 	require("DBConnect.php");
 	$conn = db_connect();
-	$sql = "INSERT INTO Video(Title,URL,Active)VALUES ('$name', 'upload_folder/$filename', 1);";
+	$sql = "INSERT INTO Video(Title,URL,Active)VALUES ('$name', 'video/$filename', 1);";
 
 	if ($conn->query($sql) === TRUE) {
-	$sql = "SELECT id FROM Video WHERE Title='$name' and URL= 'upload_folder/$filename$t'";
-	$result = $conn->query($sql);
-	$videoid=0;
-	$classid=0;
-	 if ($row = $result->fetch_assoc()){
-	$videoid= $row['id'];
-	echo $videoid;
-	foreach($_POST['classopt'] as $selected){
-	$classid=(int)$selected;
-	$sql=" INSERT INTO ClassVideo(ClassId,VideoId) VALUES($classid,$videoid);";
-	if($conn->query($sql)){
-	echo "success";
+		$sql = "SELECT id FROM Video WHERE Title='$name' and URL= 'video/$filename$t'";
+		$result = $conn->query($sql);
+		$videoid=0;
+		$classid=0;
+		if ($row = $result->fetch_assoc()){
+			$videoid= $row['id'];
+			echo $videoid;
+			foreach($_POST['classopt'] as $selected){
+				$classid=(int)$selected;
+				$sql=" INSERT INTO ClassVideo(ClassId,VideoId) VALUES($classid,$videoid);";
+				if($conn->query($sql)){
+					echo "success";
+				}
+			}
+		}
 	}
-	}
-	}}
 
 ?>
 
