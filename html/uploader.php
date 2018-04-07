847d61089c884efd08fb0a5d64319eb5325350b3
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-04-07 14:26:05 -0400

    adding fix for sessions in addclass.php

---
+++ html/uploader.php
@@ -0,0 +1,40 @@
+<?php
+	$tmp_file = $_FILES['image']['tmp_name'];
+	$filename = $_FILES['image']['name'];
+
+
+	// Directory for uploads should be as follows:
+	//		-UploadFolder/TeacherUserName/ClassCode/$filename
+	// This will ensure that we keep all files organized on server while uploading.
+$t=time();
+move_uploaded_file($tmp_file,'upload_folder/'.$filename.$t)
+
+	$name=$_POST['name'];
+	$duedate=$_POST['duedate'];
+	$runtime=$_POST['runtime'];ø
+
+ // Connect to the database
+  /*  require("DBConnect.php");
+   $conn = db_connect();
+
+	$sql = "INSERT INTO Video(Title,URL,Active)VALUES ('$name', 'upload_folder/$filename',1);";
+
+if ($conn->query($sql) === TRUE) {
+$sql = "SELECT id FROM Video WHERE Title='$name' and URL= 'upload_folder/$filename$t'";
+$result = $conn->query($sql);
+$videoid=0;
+$classid=0;
+ if ($row = $result->fetch_assoc()){
+$videoid= $row['id'];
+echo $videoid;
+foreach($_POST['classopt'] as $selected){
+$classid=(int)$selected;
+$sql=" INSERT INTO ClassVideo(ClassId,VideoId) VALUES($classid,$videoid);";
+if($conn->query($sql)){
+echo "success";
+}
+}
+}}
+*/
+?>
+
