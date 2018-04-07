1cc6b0577c67ae000aea63d08ab7dce96a30752b
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-04-07 17:55:57 -0400

    adding fix for sessions in addclass.php

--- html/uploader.php
+++ html/uploader.php
@@ -37,8 +37,8 @@
 				error_log($selected." ", 3, "/home/screener/log/php.log");
 				$classid=(int)$selected;
 				$sql=" INSERT INTO ClassVideo(ClassId,VideoId) VALUES($classid,$videoid)";
-				if($conn->query($sql){
-					error_log(" We ARE crazy ", 3, "/home/screener/log/php.log")
+				if($conn->query($sql)){
+					error_log(" We ARE crazy ", 3, "/home/screener/log/php.log");
 				}
 			}
 		}
