535704ac785d53e1944b43e88b803969968c376f
korechia <orechia_kathleen@wheatoncollege.edu>
2018-04-08 18:46:46 -0400

    this

--- html/uploader.php
+++ html/uploader.php
@@ -64,7 +64,7 @@ $sql = "INSERT INTO Video(Title,URL,Active)VALUES ('$name', '$target_address', 1
 	$sql = "INSERT INTO Video(Title,URL,Active)VALUES ('$name', '$target_address', 1);";
          error_log($conn->query($sql), 3, "/home/screener/log/php.log");
  
-	if ($conn->query($sql) === TRUE) {
+	if ($conn->query($sql) !== FALSE) {
 		 error_log("here2 ", 3, "/home/screener/log/php.log");
 		$sql = "SELECT id FROM Video WHERE URL= '$target_address';";
  		error_log("here3 ", 3, "/home/screener/log/php.log");
@@ -78,7 +78,7 @@ $sql = "INSERT INTO Video(Title,URL,Active)VALUES ('$name', '$target_address', 1
 					error_log("$selected"." ", 3, "/home/screener/log/php.log");
 					$classid=(int)$selected;
 					$sql=" INSERT INTO ClassVideo(ClassId,VideoId) VALUES($classid,$videoid)";
-					if($conn->query($sql)===TRUE){
+					if($conn->query($sql)!== FALSE){
 					
 						error_log(" We ARE crazy ", 3, "/home/screener/log/php.log");
 					}
