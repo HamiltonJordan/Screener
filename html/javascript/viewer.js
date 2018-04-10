0f8aa6e722021d776047c44e68f9ed33b5957b4d
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-04-10 16:02:05 -0400

    adding logout button

--- html/javascript/viewer.js
+++ html/javascript/viewer.js
@@ -1,14 +1,16 @@
-$.get("http://screener.onthewifi.com/check.php", "", function (response){
-	login=JSON.parse(response);
-	if (!login){window.location = "http://screener.onthewifi.com";}
-	else{
-		.get("getvideo.php?video=already&titlev=already", "", function (response){
-        	json=JSON.parse(response);
-        	//alert("already " +json.video);
-			//$url = Cookies.get('URL');
-			$('#portal').get(0).pause(); 
-			$('#video_address').attr('src', json.video);
-			$('#portal').get(0).load();
-		})
-	}
+$(document).ready(function(){
+	$.get("http://screener.onthewifi.com/check.php", "", function (response){
+		login=JSON.parse(response);
+		if (!login){window.location = "http://screener.onthewifi.com";}
+		else{
+			.get("getvideo.php?video=already&titlev=already", "", function (response){
+	        	json=JSON.parse(response);
+	        	//alert("already " +json.video);
+				//$url = Cookies.get('URL');
+				$('#portal').get(0).pause(); 
+				$('#video_address').attr('src', json.video);
+				$('#portal').get(0).load();
+			})
+		}
+	});
 });
