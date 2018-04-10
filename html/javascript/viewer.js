54277ad8c433f3572cdb600585647d9880693079
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-04-10 16:14:56 -0400

    adding logout button

--- html/javascript/viewer.js
+++ html/javascript/viewer.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 			alert("here");
 			.get("getvideo.php?video=already&titlev=already", "", function (response){
 	        	json=JSON.parse(response);
-	        	//alert("already " +json.video);
+	        	alert("already " +json.video);
 				//$url = Cookies.get('URL');
 				$('#portal').get(0).pause(); 
 				$('#video_address').attr('src', json.video);
