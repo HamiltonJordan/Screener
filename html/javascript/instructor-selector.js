d06dd60af9ec713852887f4c8ab6377402407196
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-04-18 15:04:02 -0400

    adding deleteVideo.php

--- html/javascript/instructor-selector.js
+++ html/javascript/instructor-selector.js
@@ -1,21 +1,23 @@
 //Instructor-Selector.js
 $(document).ready(function () {
 	//var myId = Cookies.get('loginId');
-	alert("hey");
+
 	//PHP - request the JSON list of film information available to user
 	$.get("http://screener.onthewifi.com/fetchClassFilms.php", "", function (response){
-		alert(response);
+		//alert("B");
 		myFilms=JSON.parse(response);
+		//myFilms = {}
+		//console.log("response: " + response);
+		//alert("myFilms: " + myFilms);
 		$last_class="null";
 		//For every film in the list returned, create a cell in the table
 		for (i = 0; i < myFilms.length; i++){
 			if(myFilms[i].ClassName != $last_class){
-				alert("a");
 				$("#entry-point").append('<tr class="table-primary"><th scope="row" colspan="4">'+myFilms[i].ClassName+'</th></tr>');
 				$last_class = myFilms[i].ClassName;
 			}
 			//$("#entry-point").append('<tr class="movie-cell"><th scope="row">'+(i+1)+'</th><td class="Title"><a href="viewer.html" id="a'+i+'">'+myFilms[i].Title+'</a></td><td>1:11</td><td>3/31/18</td></tr>');
-			$("#entry-point").append('<tr class="movie-cell"><th scope="row">'+(i+1)+'</th><td class="Title"><a href="viewer.html" id="a'+i+'">'+myFilms[i].Title+'</a></td><td>'+myFilms[i].runtime+'</td><td>'+myFilms[i].duedate+'</td><td class="delete" data-count="'+i+'"></td></tr>');
+			$("#entry-point").append('<tr class="movie-cell"><th scope="row">'+(i+1)+'</th><td class="Title"><a href="viewer.html" id="a'+i+'">'+myFilms[i].Title+'</a></td><td>'+myFilms[i].runtime+'</td><td>'+myFilms[i].duedate+'</td><td class="delete" data-count="'+i+'"><svg width="30" height="11" class="minus-frame"><rect x="2" y="1" rx="5" ry="5" width="26" height="8" class="minus-symbol" /></svg></td></tr>');
 			$("#a"+i).data("URL", myFilms[i].URL);
 		}
 		$("#a"+i).click({p1:myFilms[i].URL,p2:myFilms[i].Title},function(event){
@@ -25,7 +27,7 @@ $(document).ready(function () {
 				json=JSON.parse(response);
 			});
 		});
-		/*
+		
 		$('.delete').click(function() {
 			$c = confirm("Are you sure you want to delete this film?");
 			if ($c) {	//Delete video
@@ -34,7 +36,7 @@ $(document).ready(function () {
 				alert($vid_id);
 			}
 		});
-		*/
+		
 
 	});
 
