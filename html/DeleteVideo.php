c84c14bc007c6759a3fe68315d473a388c6b0df1
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2018-04-18 10:36:46 -0400

    working on progress bar

---
+++ html/DeleteVideo.php
@@ -0,0 +1,36 @@
+<?php
+	error_reporting(E_ALL); 
+	ini_set('display_errors',1);
+
+	// Connect to the database
+	require("DBConnect.php");
+	$conn = db_connect();
+
+	class ReturnObject {
+		public $success = false;
+		public $rowCount = 0;
+	}
+
+
+	$videoId = $_GET["videoId"];
+	$returnObj = new ReturnObject();
+
+	if($videoId != '') {
+		$query1 = mysqli_real_escape_string($conn, $videoId);
+		$query = "
+			DELETE FROM Video WHERE Id = 90;
+			DELETE FROM ClassVideo WHERE VideoId = 90;
+		";
+		if (mysqli_query($conn, $query))
+		{	
+			$returnObj->rowCount = 1;
+			$returnObj->success = true;
+			echo "Delete Successful";
+		}
+		else
+		{
+			echo "Delete Failed...";
+		}
+	}
+	echo json_encode($returnObj);
+?>
